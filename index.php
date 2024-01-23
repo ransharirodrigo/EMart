f849b1abce6b653324782e074765aeb93fa7d9f3
ransharirodrigo <139683459+ransharirodrigo@users.noreply.github.com>
2024-01-23 16:01:56 +0530

    Created the Landing page and the sign in page

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E Mart</title>
+
+    <link rel="icon" href="assets/img/e_mart_logo.png">
+</head>
+
+<body>
+
+
+</body>
+
+</html>
