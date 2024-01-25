7c8bcd3b823d4cdf5ab92db5da1929025faa0020
ransharirodrigo <139683459+ransharirodrigo@users.noreply.github.com>
2024-01-26 01:37:03 +0530

    Completed header

--- components/header.php
+++ components/header.php
@@ -16,44 +16,63 @@
     <div class="container-fluid">
         <div class="row mt-4">
 
-            <div class="d-none col-lg-5 d-lg-block">
+            <div class="d-none col-lg-6 d-lg-block">
                 <div class="row">
                     <span class="col-4 fs-6 ">0761212065</span>
-                    <span class="col-4 fs-6 ">emart@gmail.com</span>
+                    <a href="#" class="col-4 fs-6 text-decoration-none text-reset">emart@gmail.com</a>
                     <span class="col-4 fs-6 ">27/A Ward Street</span>
                 </div>
             </div>
-            <div class="col-12 col-lg-2 offset-lg-5">
+            <div class="col-5 offset-7 col-md-4 offset-md-8 col-lg-3 offset-lg-3">
                 <div class="row">
                     <div class="col-3">
-                        <i class="bi bi-person" style="color: black; font-size: 30px;"></i>
+                        <i class="bi bi-person header_top_icon"></i>
                     </div>
                     <div class="col-3">
-                        <i class="bi bi-heart" style="color: black; font-size: 30px;"></i>
+                        <i class="bi bi-heart header_top_icon"></i>
                     </div>
                     <div class="col-3">
-                        <i class="bi bi-cart-check" style="color: black; font-size: 30px;"></i>
+                        <i class="bi bi-cart-check header_top_icon"></i>
                     </div>
                 </div>
             </div>
         </div>
 
-        <hr />
+        <hr class="my-0" />
 
-        <div class="row">
-            <div class="col-3 ">
-                <img class=" w-50" src="assets/img/e_mart_logo.png" alt="">
+        <div class="row d-flex align-items-center">
+            <div class="col-3">
+                <img class="logo" src="assets/img/e_mart_logo.png" alt="">
             </div>
-            <div class="col-7">
 
+            <div class="col-6 d-none d-lg-block mt-4">
+                <div class="row">
+                    <div class="input-group mb-3">
+                        <button class="btn btn-outline-secondary dropdown-toggle " type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
+                        <ul class="dropdown-menu" style="">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Separated link</a></li>
+                        </ul>
+                        <input type="text" class="form-control" aria-label="Text input with dropdown button">
+                        <button class="btn btn-secondary" type="button" id="button-addon2">Button</button>
+                    </div>
+                </div>
             </div>
+
+            <div class="col-5 offset-4 col-lg-2  offset-lg-1">
+                <button class="btn btn-warning">Advanced Search</button>
+            </div>
+
         </div>
-
     </div>
 
-
-
-
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/script.js"></script>
 </body>
 
 </html>
