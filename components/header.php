a31c8e6026a7157ad1360e2871492a57f593eaae
ransharirodrigo <139683459+ransharirodrigo@users.noreply.github.com>
2024-01-26 02:25:36 +0530

    Small changes in header

--- components/header.php
+++ components/header.php
@@ -48,18 +48,14 @@
             <div class="col-6 d-none d-lg-block mt-4">
                 <div class="row">
                     <div class="input-group mb-3">
-                        <button class="btn btn-outline-secondary dropdown-toggle " type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
+                        <button class="btn btn-outline-secondary dropdown-toggle " type="button" data-bs-toggle="dropdown" aria-expanded="false">Categories</button>
                         <ul class="dropdown-menu" style="">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Separated link</a></li>
+                            <li><a class="dropdown-item" href="#">All</a></li>
+                            <li><a class="dropdown-item" href="#">Mobiles</a></li>
+                            <li><a class="dropdown-item" href="#">Laptops</a></li>
                         </ul>
-                        <input type="text" class="form-control" aria-label="Text input with dropdown button">
-                        <button class="btn btn-secondary" type="button" id="button-addon2">Button</button>
+                        <input type="text" class="form-control" aria-label="Text input with dropdown button" id="search">
+                        <button class="btn btn-secondary" type="button" id="button-addon2">Search</button>
                     </div>
                 </div>
             </div>
