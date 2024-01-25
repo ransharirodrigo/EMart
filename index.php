5438de508ffc739713e50c58c66c74364412ad63
ransharirodrigo <139683459+ransharirodrigo@users.noreply.github.com>
2024-01-26 03:58:18 +0530

    Landing page

--- index.php
+++ index.php
@@ -18,9 +18,34 @@
             include "components/header.php";
             ?>
 
+            <div class=" col-12 d-flex justify-content-center">
+                <img src="assets/img/welcome_image.jpg" class="w-50" alt="">
+            </div>
+
+            <div class="col-12">
+                <div class="row">
+                    <?php
+                    for ($i = 0; $i < 8; $i++) {
+                    ?>
+                        <div class="d-none d-md-block col-md-3 col-lg-2">
+                            <div class="card" >
+                                <img src="assets/img/mobile_tmp.jpeg" class="card-img-top" alt="...">
+                                <div class="card-footer">
+                                
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                    <?php
+                    }
+
+                    ?>
+                </div>
+            </div>
 
             <?php
-
             include "components/footer.php";
             ?>
 
