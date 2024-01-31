50b5a5c5b8187a16ba4aa9356f1188438fffdca8
ransharirodrigo <139683459+ransharirodrigo@users.noreply.github.com>
2024-01-31 10:32:59 +0530

    Small change in loadBestSellingItemProcess

--- App/process/loadBestSellingItemsProcess.php
+++ App/process/loadBestSellingItemsProcess.php
@@ -1,7 +1,7 @@
 <?php
 include "../../libs/connection.php";
 
-$date = date("Y-m-d");
+$date = date("Y-m");
 
 $category_name = $_GET["category_name"];
 
@@ -11,10 +11,12 @@ $query = "SELECT product.title,product_images.path FROM emart_db.top_selling_ite
   INNER JOIN emart_db.category ON category.category_id=product.category_category_id";
 
 if ($category_name != "All") {
-    $query .= " WHERE category.category_name='" . $category_name . "' AND `top_selling_items`.`date_time` LIKE '" . $date . "'";
+    $query .= " WHERE category.category_name='" . $category_name . "' AND `top_selling_items`.`date_time` LIKE '". $date ."%"."'";
+}else{
+    $query .= " WHERE `top_selling_items`.`date_time` LIKE '" . $date ."%". "' ORDER BY `top_selling_items`.`qty` LIMIT 4";
 }
 
-$query .= " ORDER BY `top_selling_items`.`qty` LIMIT 4";
+
 
 $best_selling_items_resultset = Database::execute($query);
 
@@ -30,11 +32,11 @@ if ($best_selling_items_resultset->num_rows > 0) {
                     <?php
                     if (isset($best_selling_items_array["path"])) {
                     ?>
-                        <img src="<?php echo $best_selling_items_array["path"] ?>" class="card-img-top" alt="...">
+                        <img src="<?php echo $best_selling_items_array["path"] ?>" class="card-img-top " alt="<?php echo $best_selling_items_array["title"] . "img" ?>">
                     <?php
                     } else {
                     ?>
-                        <img src="assets/img/product_images/default_product_icon.svg" class="card-img-top" alt="...">
+                        <img src="assets/img/product_images/default_product_icon.svg" class="card-img-top " alt="product.img">
                     <?php
                     }
                     ?>
