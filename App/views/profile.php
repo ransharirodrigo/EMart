4c1e3f55b8e8bc3a4aabe6c70357ab859b487ae5
ransharirodrigo <139683459+ransharirodrigo@users.noreply.github.com>
2024-02-06 23:06:41 +0530

    Profile page design

--- App/views/profile.php
+++ App/views/profile.php
@@ -26,17 +26,76 @@
                 </div>
                 <div class="row d-flex justify-content-center text-center">
                     <span class="col-2 col-md-12">ransharirodrigo@gmail.com</span>
-                </div>
+                </div> 
                 <div class="row d-flex justify-content-center text-center mt-3">
                     <button class="btn btn-warning  col-2 col-md-8 text-dark">Change Profile Image</button>
+                </div>
+                <div class="row d-flex justify-content-center text-center mt-3">
+                    <button class="btn btn-danger  col-2 col-md-8 text-white">Logout</button>
                 </div>
 
             </div>
 
             <div class="col-12 col-md-9">
 
-                <h2>Profile Settings</h2>
+                <div class="row">
+                    <h2>Profile Settings</h2>
+                </div>
 
+                <div class="row mt-3 ">
+                    <div class="col-12 col-md-5 ">
+                        <div class="row ">
+                            <span>First Name</span>
+                        </div>
+                        <div class="row mt-2">
+                            <input type="text" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-5 offset-md-1">
+                        <div class="row">
+                            <span>Last Name</span>
+                        </div>
+                        <div class="row mt-2">
+                            <input type="text" class="form-control" />
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row mt-4">
+                    <div class="col-12 col-md-11 ">
+                        <div class="row">
+                            <span>Email</span>
+                        </div>
+                        <div class="row mt-2">
+                            <input type="email" class="form-control" />
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row mt-4 ">
+                    <div class="col-12 col-md-5 ">
+                        <div class="row">
+                            <span>Mobile</span>
+                        </div>
+                        <div class="row mt-2">
+                            <input type="text" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-5 offset-md-1">
+                        <div class="row">
+                            <span>Password</span>
+                        </div>
+                        <div class="row mt-2">
+                            <input type="password" class="form-control" />
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row justify-content-center mt-5">
+                    
+                        <button class="btn btn-outline-success col-4">Save Changes</button>
+                
+                </div>
             </div>
 
         </div>
