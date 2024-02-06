44b56be4f5d344b4fe53ff124fbc1dc4130695b0
ransharirodrigo <139683459+ransharirodrigo@users.noreply.github.com>
2024-02-06 21:08:42 +0530

    Profile page left side div

--- App/views/profile.php
+++ App/views/profile.php
@@ -6,19 +6,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
 
+    <link rel="icon" href="../../assets/img/e_mart_logo.png" />
+
     <link rel="stylesheet" href="../../assets/css/bootstrap.css" />
     <link rel="stylesheet" href="../../assets/css/style.css" />
 </head>
 
 <body>
-    <div class="container-fluid">
+    <div class="container-fluid vh-100 pt-5">
         <div class="row">
-            <div class="col-12">
+
+            <div class="col-12 col-md-3">
+
+                <div class="row d-flex justify-content-center">
+                    <img src="../../assets/img/profile_images//profile_image_tmp.jpg" alt="profileImage" class="col-2 col-md-8">
+                </div>
+                <div class="row  d-flex justify-content-center text-center">
+                    <h3 class="col-2 col-md-12">Tharushi Rodrigo</h3>
+                </div>
+                <div class="row d-flex justify-content-center text-center">
+                    <span class="col-2 col-md-12">ransharirodrigo@gmail.com</span>
+                </div>
+                <div class="row d-flex justify-content-center text-center mt-3">
+                    <button class="btn btn-warning  col-2 col-md-8 text-dark">Change Profile Image</button>
+                </div>
 
             </div>
-            <div class="col-12">
+
+            <div class="col-12 col-md-9">
+
+                <h2>Profile Settings</h2>
 
             </div>
+
         </div>
     </div>
 
