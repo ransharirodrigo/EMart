176c5085213cc24f37a0ef17506e0f55a5998ad1
ransharirodrigo <139683459+ransharirodrigo@users.noreply.github.com>
2024-02-11 21:39:08 +0530

    Permission setup for accessing the profile pape and the sign in page

--- App/views/profile.php
+++ App/views/profile.php
@@ -1,106 +1,120 @@
-<!DOCTYPE html>
-<html>
+<?php
+session_start();
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile</title>
+if (isset($_SESSION["user"])) {
 
-    <link rel="icon" href="../../assets/img/e_mart_logo.png" />
+?>
+    <!DOCTYPE html>
+    <html>
 
-    <link rel="stylesheet" href="../../assets/css/bootstrap.css" />
-    <link rel="stylesheet" href="../../assets/css/style.css" />
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Profile</title>
 
-<body>
-    <div class="container-fluid vh-100 pt-5">
-        <div class="row">
+        <link rel="icon" href="../../assets/img/e_mart_logo.png" />
 
-            <div class="col-12 col-md-3">
+        <link rel="stylesheet" href="../../assets/css/bootstrap.css" />
+        <link rel="stylesheet" href="../../assets/css/style.css" />
+    </head>
 
-                <div class="row d-flex justify-content-center">
-                    <img src="../../assets/img/profile_images//profile_image_tmp.jpg" alt="profileImage" class="col-2 col-md-8">
+    <body>
+        <div class="container-fluid vh-100 pt-5">
+            <div class="row">
+
+                <div class="col-12 col-md-3">
+
+                    <div class="row d-flex justify-content-center">
+                        <img src="../../assets/img/profile_images//profile_image_tmp.jpg" alt="profileImage" class="col-2 col-md-8">
+                    </div>
+                    <div class="row  d-flex justify-content-center text-center">
+                        <h3 class="col-2 col-md-12">Tharushi Rodrigo</h3>
+                    </div>
+                    <div class="row d-flex justify-content-center text-center">
+                        <span class="col-2 col-md-12">ransharirodrigo@gmail.com</span>
+                    </div>
+                    <div class="row d-flex justify-content-center text-center mt-3">
+                        <button class="btn btn-warning  col-2 col-md-8 text-dark">Change Profile Image</button>
+                    </div>
+                    <div class="row d-flex justify-content-center text-center mt-3">
+                        <button class="btn btn-danger  col-2 col-md-8 text-white">Logout</button>
+                    </div>
+
                 </div>
-                <div class="row  d-flex justify-content-center text-center">
-                    <h3 class="col-2 col-md-12">Tharushi Rodrigo</h3>
-                </div>
-                <div class="row d-flex justify-content-center text-center">
-                    <span class="col-2 col-md-12">ransharirodrigo@gmail.com</span>
-                </div> 
-                <div class="row d-flex justify-content-center text-center mt-3">
-                    <button class="btn btn-warning  col-2 col-md-8 text-dark">Change Profile Image</button>
-                </div>
-                <div class="row d-flex justify-content-center text-center mt-3">
-                    <button class="btn btn-danger  col-2 col-md-8 text-white">Logout</button>
-                </div>
 
-            </div>
+                <div class="col-12 col-md-9">
 
-            <div class="col-12 col-md-9">
+                    <div class="row">
+                        <h2>Profile Settings</h2>
+                    </div>
 
-                <div class="row">
-                    <h2>Profile Settings</h2>
-                </div>
-
-                <div class="row mt-3 ">
-                    <div class="col-12 col-md-5 ">
-                        <div class="row ">
-                            <span>First Name</span>
+                    <div class="row mt-3 ">
+                        <div class="col-12 col-md-5 ">
+                            <div class="row ">
+                                <span>First Name</span>
+                            </div>
+                            <div class="row mt-2">
+                                <input type="text" class="form-control" />
+                            </div>
                         </div>
-                        <div class="row mt-2">
-                            <input type="text" class="form-control" />
+                        <div class="col-12 col-md-5 offset-md-1">
+                            <div class="row">
+                                <span>Last Name</span>
+                            </div>
+                            <div class="row mt-2">
+                                <input type="text" class="form-control" />
+                            </div>
                         </div>
                     </div>
-                    <div class="col-12 col-md-5 offset-md-1">
-                        <div class="row">
-                            <span>Last Name</span>
-                        </div>
-                        <div class="row mt-2">
-                            <input type="text" class="form-control" />
-                        </div>
-                    </div>
-                </div>
 
-                <div class="row mt-4">
-                    <div class="col-12 col-md-11 ">
-                        <div class="row">
-                            <span>Email</span>
-                        </div>
-                        <div class="row mt-2">
-                            <input type="email" class="form-control" />
+                    <div class="row mt-4">
+                        <div class="col-12 col-md-11 ">
+                            <div class="row">
+                                <span>Email</span>
+                            </div>
+                            <div class="row mt-2">
+                                <input type="email" class="form-control" />
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="row mt-4 ">
-                    <div class="col-12 col-md-5 ">
-                        <div class="row">
-                            <span>Mobile</span>
+                    <div class="row mt-4 ">
+                        <div class="col-12 col-md-5 ">
+                            <div class="row">
+                                <span>Mobile</span>
+                            </div>
+                            <div class="row mt-2">
+                                <input type="text" class="form-control" />
+                            </div>
                         </div>
-                        <div class="row mt-2">
-                            <input type="text" class="form-control" />
+                        <div class="col-12 col-md-5 offset-md-1">
+                            <div class="row">
+                                <span>Password</span>
+                            </div>
+                            <div class="row mt-2">
+                                <input type="password" class="form-control" />
+                            </div>
                         </div>
                     </div>
-                    <div class="col-12 col-md-5 offset-md-1">
-                        <div class="row">
-                            <span>Password</span>
-                        </div>
-                        <div class="row mt-2">
-                            <input type="password" class="form-control" />
-                        </div>
-                    </div>
-                </div>
 
-                <div class="row justify-content-center mt-5">
-                    
+                    <div class="row justify-content-center mt-5">
+
                         <button class="btn btn-outline-success col-4">Save Changes</button>
-                
+
+                    </div>
                 </div>
+
             </div>
-
         </div>
-    </div>
 
-</body>
+    </body>
 
-</html>
+    </html>
+<?php
+
+
+} else {
+
+    header("Location:../../index.php");
+
+} ?>
--- App/views/signIn.php
+++ App/views/signIn.php
@@ -1,112 +1,124 @@
-<!DOCTYPE html>
+<?php
+session_start();
 
-<html>
+if (isset($_SESSION["user"])) {
+    header("Location:../../index.php");
+} else {
+?>
+    <!DOCTYPE html>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E Mart Sign In</title>
+    <html>
 
-    <link rel="icon" href="../../assets/img/e_mart_logo.png" />
-    <link rel="stylesheet" href="../../assets/css/bootstrap.css" />
-    <link rel="stylesheet" href="../../assets/css/style.css" />
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>E Mart Sign In</title>
 
-<body>
-    <div class="container-fluid">
-        <div class="row d-flex justify-content-center align-items-center vh-100">
-            <div class="col-12">
-                <div class="row">
-                    <div class=" d-flex justify-content-center ">
-                        <img class="logo" src="../../assets/img/e_mart_logo.png" alt="">
-                    </div>
-                </div>
-            </div>
+        <link rel="icon" href="../../assets/img/e_mart_logo.png" />
+        <link rel="stylesheet" href="../../assets/css/bootstrap.css" />
+        <link rel="stylesheet" href="../../assets/css/style.css" />
+    </head>
 
-            <div class="col-8 col-sm-6 col-lg-5  border py-3 rounded-3">
-
-                <div class="row text-center">
-                    <div>
-                        <h3>Sign In</h3>
-                    </div>
-                </div>
-
-                <div class="row mt-4">
-                    <div class="col-12 col-lg-3">
-                        <span>Email</span>
-                    </div>
-                    <div class="col-12 col-lg-9">
-                        <?php
-                        if (isset($_COOKIE["email"])) {
-                        ?>
-                            <input type="email" class="form-control" id="email" value="<?php echo $_COOKIE["email"] ?>" />
-                        <?php
-                        } else {
-                        ?>
-                            <input type="email" class="form-control" id="email" value="" />
-                        <?php
-                        }
-                        ?>
-
-                    </div>
-                </div>
-                <div class="row mt-4">
-                    <div class="col-12 col-lg-3">
-                        <span>Password</span>
-                    </div>
-                    <div class="col-12 col-lg-9">
-                        <input type="password" class="form-control" id="password" />
-                    </div>
-                </div>
-
-                <div class="row row-gap-4 mt-4">
-                    <div class="col-12 col-xl-7">
-                        <div class="row">
-
-                            <input type="checkbox" class="col-1 offset-xl-1" id="remember_me" />
-                            <span class="col-6">Remember Me</span>
-
-                        </div>
-                    </div>
-                    <div class="col-12 col-xl-5">
-                        <a href="">Forgot Password</a>
-                    </div>
-                </div>
-
-                <div class="row justify-content-center mt-4">
-                    <button class="btn btn-warning col-6" onclick="signIn();">Sign In</button>
-                </div>
-
-            </div>
-
-            <div class="col-12 ">
-                <div class="row justify-content-center">
-                    <div class="col-8 col-sm-6 col-lg-5">
-                        <div class="row align-items-center text-center">
-                            <hr class="col-2 col-sm-3 col-lg-4 " />
-                            <p class="col-8 col-sm-6 col-lg-4">New to EMart ?</p>
-                            <hr class="col-2 col-sm-3 col-lg-4 " />
+    <body>
+        <div class="container-fluid">
+            <div class="row d-flex justify-content-center align-items-center vh-100">
+                <div class="col-12">
+                    <div class="row">
+                        <div class=" d-flex justify-content-center ">
+                            <img class="logo" src="../../assets/img/e_mart_logo.png" alt="">
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="col-12 ">
-                <div class="row justify-content-center">
-                    <div class="col-8 col-sm-6 col-lg-3">
-                        <div class="row justify-content-center">
-                            <a href="signUp.php" class="text-decoration-none text-reset">
-                                <button class="btn btn-outline-secondary col-8">Create Account</button>
-                            </a>
+                <div class="col-8 col-sm-6 col-lg-5  border py-3 rounded-3">
+
+                    <div class="row text-center">
+                        <div>
+                            <h3>Sign In</h3>
+                        </div>
+                    </div>
+
+                    <div class="row mt-4">
+                        <div class="col-12 col-lg-3">
+                            <span>Email</span>
+                        </div>
+                        <div class="col-12 col-lg-9">
+                            <?php
+                            if (isset($_COOKIE["email"])) {
+                            ?>
+                                <input type="email" class="form-control" id="email" value="<?php echo $_COOKIE["email"] ?>" />
+                            <?php
+                            } else {
+                            ?>
+                                <input type="email" class="form-control" id="email" value="" />
+                            <?php
+                            }
+                            ?>
+
+                        </div>
+                    </div>
+                    <div class="row mt-4">
+                        <div class="col-12 col-lg-3">
+                            <span>Password</span>
+                        </div>
+                        <div class="col-12 col-lg-9">
+                            <input type="password" class="form-control" id="password" />
+                        </div>
+                    </div>
+
+                    <div class="row row-gap-4 mt-4">
+                        <div class="col-12 col-xl-7">
+                            <div class="row">
+
+                                <input type="checkbox" class="col-1 offset-xl-1" id="remember_me" />
+                                <span class="col-6">Remember Me</span>
+
+                            </div>
+                        </div>
+                        <div class="col-12 col-xl-5">
+                            <a href="">Forgot Password</a>
+                        </div>
+                    </div>
+
+                    <div class="row justify-content-center mt-4">
+                        <button class="btn btn-warning col-6" onclick="signIn();">Sign In</button>
+                    </div>
+
+                </div>
+
+                <div class="col-12 ">
+                    <div class="row justify-content-center">
+                        <div class="col-8 col-sm-6 col-lg-5">
+                            <div class="row align-items-center text-center">
+                                <hr class="col-2 col-sm-3 col-lg-4 " />
+                                <p class="col-8 col-sm-6 col-lg-4">New to EMart ?</p>
+                                <hr class="col-2 col-sm-3 col-lg-4 " />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
+                <div class="col-12 ">
+                    <div class="row justify-content-center">
+                        <div class="col-8 col-sm-6 col-lg-3">
+                            <div class="row justify-content-center">
+                                <a href="signUp.php" class="text-decoration-none text-reset">
+                                    <button class="btn btn-outline-secondary col-8">Create Account</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
-    </div>
 
-    <script src="../../assets/js/script.js"></script>
-</body>
+        <script src="../../assets/js/script.js"></script>
+    </body>
 
-</html>
+    </html>
+
+<?php
+}
+
+?>
