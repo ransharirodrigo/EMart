ff355f263f2f78ef2ab832252dd4ddaab89c3dc1
ransharirodrigo <139683459+ransharirodrigo@users.noreply.github.com>
2024-02-25 11:20:45 +0530

    Category page design

--- App/views/categoryPage.php
+++ App/views/categoryPage.php
@@ -24,12 +24,34 @@ if (isset($_GET['id'])) {
                 include(BASE_PATH . '/components/header.php');
                 ?>
 
-                <div class="col-1  ">
+                <!-- <div class="col-1  ">
                     <select name="" id="" class="form-select">
                         <option value="">Default Sort</option>
                         <option value="">Name</option>
                     </select>
+                </div> -->
+
+                <div class="col-12 mb-5">
+                    <div class="row d-flex justify-content-center ">
+
+                        <?php
+                        for ($i = 0; $i < 10; $i++) {
+                        ?>
+                            <div class="col-8 col-md-6 col-lg-3 d-flex justify-content-center mt-3">
+                                <img src="../../assets/img/product_images/iphone_11.jpeg" class="category_page_product_image" alt="product image" />
+                            </div>
+
+                        <?php
+                        }
+                        ?>
+
+                    </div>
                 </div>
+
+
+                <?php
+                include "../../components/footer.php";
+                ?>
 
             </div>
         </div>
