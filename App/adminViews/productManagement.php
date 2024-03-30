50dea0bd55a0b49717c93e5f5c62e5a032649ef2
ransharirodrigo <139683459+ransharirodrigo@users.noreply.github.com>
2024-03-30 10:47:30 +0530

    Product management UI

--- App/adminViews/productManagement.php
+++ App/adminViews/productManagement.php
@@ -4,8 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Product Management</title>
+
     <link rel="icon" href="../../assets/img/e_mart_logo.png" />
+    <link rel="icon" href="../../assets/css/style.css" />
+
+    <title>Product Management</title>
 </head>
 
 <body>
@@ -17,16 +20,172 @@
             include("../../components/adminHeader.php");
             ?>
         </div>
-        <div class="row mt-3" >
+        <div class="row mt-3">
+
             <div class="col-md-6 offset-md-3">
-              
-                    <div class="input-group">
-                        <input type="text" class="form-control form-control-lg" name="query" placeholder="Search products..." style="width: 400px;">
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-warning btn-lg" type="submit">Search</button>
+                <div class="input-group">
+                    <input type="text" class="form-control form-control-lg" name="query" placeholder="Search products..." style="width: 400px;">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-warning btn-lg" type="submit">Search</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-10 offset-md-1 mt-5">
+                <div class="row mt-3">
+                    <div class="col-md-12">
+                        <div class="table-container">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Product ID</th>
+                                        <th>Title</th>
+                                        <th>Product Status</th>
+                                        <th>Quantity</th>
+                                        <th>Price</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Product 1</td>
+                                        <td>Active</td>
+                                        <td>10</td>
+                                        <td>$20.00</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Product 1</td>
+                                        <td>Active</td>
+                                        <td>10</td>
+                                        <td>$20.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Product 1</td>
+                                        <td>Active</td>
+                                        <td>10</td>
+                                        <td>$20.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Product 1</td>
+                                        <td>Active</td>
+                                        <td>10</td>
+                                        <td>$20.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Product 1</td>
+                                        <td>Active</td>
+                                        <td>10</td>
+                                        <td>$20.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Product 1</td>
+                                        <td>Active</td>
+                                        <td>10</td>
+                                        <td>$20.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Product 1</td>
+                                        <td>Active</td>
+                                        <td>10</td>
+                                        <td>$20.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Product 1</td>
+                                        <td>Active</td>
+                                        <td>10</td>
+                                        <td>$20.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Product 1</td>
+                                        <td>Active</td>
+                                        <td>10</td>
+                                        <td>$20.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Product 1</td>
+                                        <td>Active</td>
+                                        <td>10</td>
+                                        <td>$20.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Product 1</td>
+                                        <td>Active</td>
+                                        <td>10</td>
+                                        <td>$20.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Product 1</td>
+                                        <td>Active</td>
+                                        <td>10</td>
+                                        <td>$20.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Product 1</td>
+                                        <td>Active</td>
+                                        <td>10</td>
+                                        <td>$20.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Product 1</td>
+                                        <td>Active</td>
+                                        <td>10</td>
+                                        <td>$20.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Product 1</td>
+                                        <td>Active</td>
+                                        <td>10</td>
+                                        <td>$20.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Product 1</td>
+                                        <td>Active</td>
+                                        <td>10</td>
+                                        <td>$20.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Product 1</td>
+                                        <td>Active</td>
+                                        <td>10</td>
+                                        <td>$20.00</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
-              
+                </div>
+
+            </div>
+
+            <div class="col-md-10 offset-md-1">
+                <div class="row mt-3">
+                    <div class="col-10 col-md-4">
+                        <button class="btn btn-primary btn-lg btn-block mb-2">Add New Product</button>
+                    </div>
+                    <div class="col-10 col-md-4">
+                        <button class="btn btn-success btn-lg btn-block mb-2">Update Product</button>
+                    </div>
+                    <div class="col-10 col-md-4">
+                        <button class="btn btn-danger btn-lg btn-block mb-2">Disable Product</button>
+                    </div>
+                </div>
             </div>
         </div>
 
